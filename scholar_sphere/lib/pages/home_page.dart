89f020d3c649ea2176bcbab14138c8803af5c3ca
CarrotNinja <r-sherwani@outlook.com>
2024-06-09 23:54:04 -0400

    Scholar Sphere Init!

---
+++ scholar_sphere/lib/pages/home_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue[800],
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              children: [
+                //greetings row
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    //Hi name
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Hi Razam!",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          '8 June, 2024',
+                          style: TextStyle(color: Colors.blue[200]),
+                        )
+                      ],
+                    ),
+                    //Noti
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.blue[600],
+                          borderRadius: BorderRadius.circular(12)),
+                      padding: const EdgeInsets.all(12),
+                      child: const Icon(
+                        Icons.notifications,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+
+                //search bar
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue[600],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: EdgeInsets.all(12),
+                  child: Row(children: [
+                    Icon(Icons.search, color: Colors.white),
+                    SizedBox(width: 5),
+                    Text(
+                      'Search',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ]),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+
+                //How do you feel?
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "How do you feel?",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Icon(
+                      Icons.more_horiz,
+                      color:Colors.white,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
